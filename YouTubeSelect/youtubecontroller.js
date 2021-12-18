a3dd4fe160eb995a0cb0dcec9f1f64434f3807c0
luvis12 <luvis1206@gmail.com>
2021-12-18 12:33:58 +0800

    Update youtubecontroller.js

--- YouTubeSelect/youtubecontroller.js
+++ YouTubeSelect/youtubecontroller.js
@@ -144,11 +144,11 @@ const BASE_URL = 'https://www.youtube.com/premium'
     }
     
     if (isOK.includes(rootName) == true) {
-      panel['content'] = `YouTube节点正常  地区：${regData[rootName]}`
+      panel['content'] = `YouTube节点Premium正常  地区：${regData[rootName]}`
       panel['icon'] = params.icon1
       panel['icon-color'] = params.color1
     } else {
-      panel['content'] = `节点异常 你可能无法使用会员权益`
+      panel['content'] = `节点送中 无广告且无法使用会员权益`
       panel['icon'] = params.icon2
       panel['icon-color'] = params.color2
     }
