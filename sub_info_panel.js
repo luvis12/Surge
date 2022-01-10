e76cce14fd473e40bbb7fc182b6ee0dae585826a
luvis12 <luvis1206@gmail.com>
2022-01-10 10:00:45 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -53,7 +53,7 @@ Sub_info = script-name=Sub_info,update-interval=600
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} | ${day}`,
+    title: `${args.title} | ${now}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
