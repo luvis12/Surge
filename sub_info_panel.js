702786588388ec8feb8445653c05d28f53308798
luvis12 <luvis1206@gmail.com>
2022-01-10 10:43:17 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -46,17 +46,13 @@ Sub_info = script-name=Sub_info,update-interval=600
   }
 
   let now = new Date();
-  let month = now.getMonth();
-  let year = now.getFullYear();
-  let today = now.getdate();
-
   let hour = now.getHours();
   let minutes = now.getMinutes();
   hour = hour > 9 ? hour : "0" + hour;
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} | ${today} ${hour}:${minutes}`,
+    title: `${args.title} | ${hour}:${minutes}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
